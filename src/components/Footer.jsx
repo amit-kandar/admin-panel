f907b77fff1e0e1fb2ddfeb59bf20e8af965342c
Amit Kandar <kandaramit2001@gmail.com>
2024-03-13 12:02:40 +0530

    Improve the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <footer className='w-full h-20 flex items-center border border-gray-500 mt-5 px-10 rounded-md text-gray-600 dark:text-gray-400'>
+            <div className='w-full flex items-center justify-between'>
+                <span className=''>Designed By <Link to="https://metawebtx.com/">@Metawebtx</Link>. All rights reserved.</span>
+                <span className='space-x-5'>
+                    <Link to="/terms">Terms</Link>
+                    <Link to="/privacy">Privacy</Link>
+                </span>
+            </div>
+        </footer>
     )
 }
 
