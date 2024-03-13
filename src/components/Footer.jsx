17337066b8c33e39c867d41d62f636112c02af85
Amit Kandar <kandaramit2001@gmail.com>
2024-03-13 12:35:33 +0530

    fix the footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <footer className='w-full h-20 flex items-center border border-gray-500 mt-5 px-10 rounded-md text-gray-600 dark:text-gray-400'>
-            <div className='w-full flex items-center justify-between'>
-                <span className=''>Designed By <Link to="https://metawebtx.com/">@Metawebtx</Link>. All rights reserved.</span>
-                <span className='space-x-5'>
+        <footer className='w-full flex items-center border border-gray-500 mt-5 p-3 sm:p-5 rounded-md text-gray-600 dark:text-gray-400 mb-5'>
+            <div className='w-full flex items-center justify-between flex-col gap-3 sm:flex-row'>
+                <span className='text-sm'>Designed By <Link to="https://metawebtx.com/">@Metawebtx</Link>. All rights reserved.</span>
+                <span className='space-x-5 w-fit flex justify-between'>
                     <Link to="/terms">Terms</Link>
                     <Link to="/privacy">Privacy</Link>
                 </span>
